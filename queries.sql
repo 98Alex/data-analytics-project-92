d6cda09ebeb948584c3fbc035b5fc0c8d9de1890
98Alex <moryak1998@mail.ru>
2024-08-02 00:59:45 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 -- Запрос на всю таблицу customers
-select count(customer_id)
+select count(distinct(customer_id))
 from customers;
 
 -- Запрос для поиска продавцов с маленькой средней выручкой
@@ -51,7 +51,6 @@ select
         when age between 16 and 25 then '16-25'
         when age between 26 and 40 then '26-40'
         when age > 40 then '40+'
-        else 'Unknown'
     end as age_category,
     count(customer_id) as age_count
 from customers
@@ -69,13 +68,33 @@ group by to_char(s.sale_date, 'YYYY-MM')
 order by selling_month;
 
 -- Запрос на поиск покупателей, чья первая покупка пришлась на акцию
-select distinct on (s.customer_id)
-    c.first_name || ' ' || c.last_name as customer,
-    s.sale_date,
-    e.first_name || ' ' || e.last_name as seller
-from sales as s
-inner join products as p on s.product_id = p.product_id
-inner join employees as e on s.sales_person_id = e.employee_id
-inner join customers as c on s.customer_id = c.customer_id
-where p.price = 0
-order by s.customer_id, s.sale_date;
+with ranked_sales as (
+    select
+        s.customer_id,
+        s.sale_date,
+        c.first_name || ' ' || c.last_name as customer,
+        e.first_name || ' ' || e.last_name as seller,
+        sum(p.price * s.quantity) as total_sum,
+        row_number()
+            over (partition by s.customer_id order by s.sale_date)
+        as rn
+    from sales as s
+    inner join products as p on s.product_id = p.product_id
+    inner join employees as e on s.sales_person_id = e.employee_id
+    inner join customers as c on s.customer_id = c.customer_id
+    group by
+        s.customer_id,
+        c.first_name,
+        c.last_name,
+        s.sale_date,
+        e.first_name,
+        e.last_name
+)
+
+select
+    customer,
+    sale_date,
+    seller
+from ranked_sales
+where total_sum = 0 and rn = 1
+order by customer_id;
